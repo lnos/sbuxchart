45d92423ec46604aa488561ad3a273c9ffd8fe26
Seong Kim <hoon5421@hotmail.com>
2018-09-29 22:49:31 -0400

    Updating data structure

--- src/Drinks.js
+++ src/Drinks.js
@@ -3,16 +3,24 @@ import Drink from './Drink.js'
 import './Drinks.css';
 
 class Drinks extends Component{
-    
+
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
         this.state = {add: ''};
+        var EspressoMacchiato = {name: 'Latte macchiato', src: './img/LM.jpg', caffeine: [150,225,225]};
+        var CaramelMacchiato = {name: 'Caramel macchiato', src: './img/CM.jpg', caffeine: [75,150,150]};
+        var CoffeeFrapuccino = {name: 'Coffee Frapuccino', src: './img/CF.jpg', caffeine: [70,95,130]};
+        var FlatWhite = {name: 'Flat White', src: './img/FW.jpg', caffeine: [130,195,195]};
       }
 
     handleClick(name){
         this.setState({add: name});
         console.log(name);
+    }
+
+    displayDrinks(data){
+        
     }
 
     render(){
